99a08783b6a5160a43c027937d09684d546acd60
Irawan Kilmer <{58170912}+{irawankilmer}@users.noreply.github.com>
2024-01-19 11:23:49 +0700

    ss

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,9 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   ignoredRouteFiles: ["**/.*"],
+  future: {
+    tailwind: true
+  }
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
